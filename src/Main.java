68109f16e6da33ec2f31cf0068f4ee6040718e84
Gustavo <gstvcaixeta@gmail.com>
2024-09-23 13:26:25 -0300

    add funcoes de inscricao e progressao em bootcamps e conteudos

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 public class Main {
     public static void main(String[] args) {
         Curso curso = new Curso("Java", "Se aprimore em mais um curso de Java", 14);
 
-        System.out.println(curso);
-
         Mentoria mentoria = new Mentoria("Angular", "Mergulhe em profundidade em Angular");
 
-        System.out.println(mentoria);
+        Conteudo conteudo = new Mentoria("Nova mentoria", "gosto muito dessa");
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Melhor do Brasil");
+        bootcamp.setDescricao("Bootcamp completo para amantes da Programacao");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(conteudo);
+
+        Dev devGustavo = new Dev("Gustavo");
+        devGustavo.inscreverBootcamp(bootcamp);
+        System.out.println(devGustavo.getConteudosInscritos());
+        devGustavo.progredir();
+
+        System.out.println(devGustavo.getConteudosInscritos());
+        System.out.println(devGustavo.getConteudosConcluidos());
+        Dev devFernando = new Dev("Fernando");
+
+
+//        System.out.println(bootcamp.getConteudos());
     }
 }
