230cfcb324fe58ff09b3350b0cb1a40d3a74d548
Gustavo <gstvcaixeta@gmail.com>
2024-09-23 12:44:47 -0300

    modelagem entidades curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso("Java", "Se aprimore em mais um curso de Java", 14);
+
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria("Angular", "Mergulhe em profundidade em Angular");
+
+        System.out.println(mentoria);
     }
 }
